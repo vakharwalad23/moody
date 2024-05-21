d43846e52bb000f8c87d7e77d53abd6ef55e5f5e
vakharwalad23 <vakharwala.d@gmail.com>
2024-05-22 04:29:08 +0530

    feat: added user creation functionality

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,9 +1,27 @@
-import React from "react";
+import { prisma } from "@/utils/db";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const createUser = async () => {
+  const user = await currentUser();
+  console.log(user);
+  const match = await prisma.user.findUnique({
+    where: { clerkId: user?.id as string },
+  });
+  if (!match) {
+    const newUser = await prisma.user.create({
+      data: {
+        clerkId: user?.id as string,
+        email: user?.emailAddresses[0].emailAddress as string,
+      },
+    });
+  }
+  redirect("/journal");
+};
 
 type Props = {};
-
-function NewUser({}: Props) {
-  return <div>NewUser</div>;
+async function NewUser({}: Props) {
+  await createUser();
 }
 
 export default NewUser;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 import Link from "next/link";
+import { auth } from "@clerk/nextjs/server";
 
-export default function Home() {
+export default async function Home() {
+  const { userId }: { userId: string | null } = auth();
+  let href = userId ? "/journal" : "/new-user";
+
   return (
     <div className="px-5 w-screen h-screen bg-black flex justify-center items-center text-white space-y-5">
       <div className="w-full max-w-[900px] mx-auto">
@@ -10,7 +14,7 @@ export default function Home() {
           with your journals.
         </p>
         <div className="flex">
-          <Link href="/journal">
+          <Link href={href}>
             <button className="py-3 px-3 border border-gray-400 rounded-lg shadow-md transition-all duration-300 hover:shadow-lg hover:translate-y-[-3px] hover:bg-gray-500/30">
               Get Started
             </button>
