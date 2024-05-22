95960d246441050b03271218a88bd3858ae46c1a
vakharwalad23 <vakharwala.d@gmail.com>
2024-05-23 04:17:58 +0530

    chore: update Prisma client log level to only warn and error

--- utils/db.ts
+++ utils/db.ts
@@ -4,7 +4,7 @@ import { PrismaClient } from '@prisma/client'
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined }
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient({
-    log: ['query', 'info', 'warn'],
+    log: ['warn', 'error'],
 })
 
 
