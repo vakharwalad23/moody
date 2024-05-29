f3c2b0ef3ef6e76b5f4d0c6a9c9087366db8b895
vakharwalad23 <vakharwala.d@gmail.com>
2024-05-30 02:42:35 +0530

    feat: added navigation links to dashboard layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,14 +1,27 @@
 import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
 
 type Props = {
   children: React.ReactNode;
 };
 
+const links = [
+  { href: "/", label: "Home" },
+  { href: "/journal", label: "Journal" },
+];
+
 function DashBoardLayout(props: Props) {
   return (
     <div className="h-screen w-screen relative">
       <aside className="absolute w-[200px] top-0 left-0 h-full border-r border-gray-800/12">
-        MooDy
+        <div>MooDy</div>
+        <ul>
+          {links.map((link) => (
+            <li key={link.href} className="px-2 py-6 text-xl">
+              <Link href={link.href}>{link.label}</Link>
+            </li>
+          ))}
+        </ul>
       </aside>
       <div className="ml-[200px] h-full">
         <header className="h-[60px] border-b border-gray-800/12">
