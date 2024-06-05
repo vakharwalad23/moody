9a7acbc032f41331357fa6630e97983cf4ed52a3
vakharwalad23 <vakharwala.d@gmail.com>
2024-06-06 04:26:23 +0530

    chore: update .env.example and sign-in page for clerk deployment

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,11 @@ import { SignIn } from "@clerk/nextjs";
 export default function SignInPage() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <SignIn path="/sign-in" signUpUrl="/sign-up" />
+      <SignIn
+        path="/sign-in"
+        forceRedirectUrl="/new-user"
+        signUpUrl="/sign-up"
+      />
     </div>
   );
 }
