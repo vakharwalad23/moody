038850605868187865764acf2f5064b59d7a1d35
vakharwalad23 <vakharwala.d@gmail.com>
2024-06-15 17:30:23 +0530

    chore: update loading states

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,17 +2,14 @@ import React from "react";
 
 export default function Loading() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-white">
+    <div className="flex items-center justify-center min-h-screen">
       <div className="text-center">
         <div className="flex justify-center mb-4">
-          <span className="w-3 h-3 bg-black rounded-full mx-1 animate-bounce"></span>
-          <span className="w-3 h-3 bg-black rounded-full mx-1 animate-bounce animation-delay-200"></span>
-          <span className="w-3 h-3 bg-black rounded-full mx-1 animate-bounce animation-delay-400"></span>
-          <span className="w-3 h-3 bg-black rounded-full mx-1 animate-bounce animation-delay-600"></span>
+          <span className="w-6 h-6 rounded-full bg-black animate-pulse mx-1"></span>
+          <span className="w-6 h-6 rounded-full bg-black animate-pulse mx-1 animation-delay-200"></span>
+          <span className="w-6 h-6 rounded-full bg-black animate-pulse mx-1 animation-delay-400"></span>
         </div>
-        <p className="text-black font-semibold text-2xl animate-bounce">
-          Loading
-        </p>
+        <h1 className="font-bold text-4xl mb-2 animate-bounce">MooDy</h1>
       </div>
     </div>
   );
--- components/EntryCard/NewEntryCard.tsx
+++ components/EntryCard/NewEntryCard.tsx
@@ -7,16 +7,42 @@ import { useRouter } from "next/navigation";
 type Props = {};
 
 const NewEntryCard = (props: Props) => {
+  const [loading, setLoading] = React.useState(false);
   const router = useRouter();
 
   const handleClick = async () => {
+    setLoading(true);
     const data = await createNewEntry();
+    setLoading(false);
     router.push(`/journal/${data.id}`);
   };
   return (
     <div className="cursor-pointer overflow-hidden rounded-lg bg-white shadow-lg">
       <div className="px-4 py-5 sm:p-6" onClick={handleClick}>
         <span className="text-2xl">New Entry</span>
+        {loading && (
+          <div className="flex pt-2">
+            <svg
+              className="animate-spin h-5 w-5 mr-3 text-gray-500"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+            <span className="text-gray-500">Loading...</span>
+          </div>
+        )}
       </div>
     </div>
   );
