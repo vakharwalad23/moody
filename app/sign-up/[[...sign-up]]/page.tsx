5f84088b5ba45c8eb1de1a5312ccf38c15c9cbab
vakharwalad23 <vakharwala.d@gmail.com>
2024-06-15 11:27:35 +0530

    chore: Update sign-in and sign-up pages for Clerk deployment

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -3,11 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function SignUpPage() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <SignUp
-        path="/sign-up"
-        signInUrl="/sign-in"
-        forceRedirectUrl="/journal"
-      />
+      <SignUp path="/sign-up" signInUrl="/sign-in" />
     </div>
   );
 }
