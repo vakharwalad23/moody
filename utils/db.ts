9e64b44e05e7c337ab56f39747587354f535024e
vakharwalad23 <vakharwala.d@gmail.com>
2025-08-29 15:36:39 +0530

    fix: prisma edge client

--- utils/db.ts
+++ utils/db.ts
@@ -1,14 +1,17 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client/edge";
 
 // Prevent multiple instances of Prisma Client in development
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined }
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({
-    log: ['warn', 'error'],
-})
-
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["warn", "error"],
+  });
 
 // This is a workaround to prevent Prisma from being initialized multiple times in development
-if (process.env.NODE_ENV === 'development') {
-    globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV === "development") {
+  globalForPrisma.prisma = prisma;
 }
