b86d09695eb5776aac71d11bbfa1f9012fed5c8a
vakharwalad23 <vakharwala.d@gmail.com>
2025-09-01 03:33:23 +0530

    fix: reorder imports and clean up unused journal entry retrieval code

--- app/api/question/route.ts
+++ app/api/question/route.ts
@@ -1,25 +1,14 @@
-import { QA } from "@/utils/ai"
-import { getUserByClerkId } from "@/utils/auth"
-import { prisma } from "@/utils/db"
-import { NextResponse } from "next/server"
-import { User } from "@/utils/definitions"
+import { getUserByClerkId } from "@/utils/auth";
+import { prisma } from "@/utils/db";
+import { QA } from "@/utils/ai";
+import { NextResponse } from "next/server";
+import { User } from "@/utils/definitions";
 
 export const POST = async (request: Request) => {
-    const { question }: { question: string } = await request.json()
-    const user = await getUserByClerkId() as User;
+  const { question }: { question: string } = await request.json();
+  const user = (await getUserByClerkId()) as User;
 
-    const entries = await prisma.journalEntry.findMany({
-        where: {
-            userId: user.id
-        },
-        select :{
-            id: true,
-            content: true,
-            createdAt: true,
-        }
-    })
+  const answer = await QA(question, user.id);
 
-    const answer = await QA(question, entries)
-
-    return NextResponse.json({ data: answer })
-}
+  return NextResponse.json({ data: answer });
+};
